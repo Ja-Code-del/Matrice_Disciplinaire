410f9c0a095fcf5c2f97fa20f22e7bb322ff8427
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-10-25 14:46:11 +0000

    update

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import sys
 from src.ui.main_window import MainGendarmeApp
 from PyQt6.QtWidgets import QApplication
 
+
 def main():
     app = QApplication(sys.argv)
     window = MainGendarmeApp()
     window.show()
     sys.exit(app.exec())
+
 
 if __name__ == '__main__':
     main()
